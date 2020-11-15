402efae630ca391bfbf2d9ba8f5941983951797f
dns02023 <dns02023@naver.com>
2020-11-16 00:26:34 +0900

    수정

--- recommender/urls.py
+++ recommender/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^similarity', Similarity.as_view(), name='similarity'),
-    url(r'^cbrecommend/(?P<id>[0-9]+)/', CBRecommend.as_view(), name='cbrecommend'),
+    url(r'^cbrecommend/(?P<id>[0-9]+)', CBRecommend.as_view(), name='cbrecommend'),
     url(r'^train', Train.as_view(), name='train'),
     url(r'^cfrecommend', CFRecommend.as_view(), name='cfrecommend'),
 ]
